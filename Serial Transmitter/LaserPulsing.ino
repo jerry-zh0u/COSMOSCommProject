3bd68150986e8da177e0ccdb9b2adb09fcefaab6
jerry-zh0u <152764900+jerry-zh0u@users.noreply.github.com>
2024-07-31 23:34:26 -0700

    Update LaserPulsing.ino

--- Serial Transmitter/LaserPulsing.ino
+++ Serial Transmitter/LaserPulsing.ino
@@ -1,7 +1,9 @@
 #include <SoftwareSerial.h>
 
+#define DELAYFACTOR 50
+#define SPACEDELAY 20
 
-SoftwareSerial mySerial[3] = {SoftwareSerial(2, 3), SoftwareSerial(4, 5), SoftwareSerial(6, 7)};
+SoftwareSerial mySerial[2] = {SoftwareSerial(2, 3), SoftwareSerial(6, 7)};
 
 int p = 0;
 bool read[3] = {false, false, false}; 
@@ -10,9 +12,10 @@ String s[3];
 
 void setup() {
   Serial.begin(9600); 
-  for(int i = 0; i < 3; i ++){
-    mySerial[i].begin(300);
-  }
+  mySerial[0].begin(300);
+  mySerial[1].begin(300);
+
+  p = 0;
 }
 
 void loop() {
@@ -24,12 +27,36 @@ void loop() {
     if(!read[i]){
       s[i] = Serial.readString();
       read[i] = true;
+      p = 0;
     }
   }
   for(int i = 0; i < 3; i++){
-    mySerial[i].println(s[i]);
+    if(i == 1){
+      while(p < s[1].length()){
+        if(s[1].charAt(p) == '\n'){
+          digitalWrite(5, LOW);
+        }else{
+          digitalWrite(5, HIGH);
+          delay((s[1].charAt(p) - 32 + 1) * DELAYFACTOR);
+        }
+        digitalWrite(5, LOW);
+        delay(500);
+        p++;
+      }
+    }else{
+      if(i == 2){
+        realIDX = 1;
+      }
+
+      mySerial[realIDX].print(" ");
+      for(int j = 0; j < s[realIDX].length(); j+=5){
+        mySerial[realIDX].print(s[i].substring(j, j + 5));
+      }
+    }
+    
     Serial.print("Word "); Serial.print(i); Serial.println(": ");
     Serial.println(s[i]);
     read[i] = false;
   }
+  p = 0;
 }
